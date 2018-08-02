79cdfed470e69843841c645dd6acb27dce6ded0c
leodotng <josh@leo.ng>
2018-08-01 22:08:07 -0600

    added vuesax UI framework

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import Vuesax from 'vuesax'
 import App from './App.vue'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax)
 
 Vue.config.productionTip = false
 
